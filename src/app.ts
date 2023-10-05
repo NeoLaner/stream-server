f71f1461ba66bf32afc9ed026aceecfea867790b
Neo <neo.laner.official@gmail.com>
2023-10-05 17:13:26 +0330

    init
    Former-commit-id: 8b875b4c35b971ab985d3283363acb681f830c15

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import bodyParser from "body-parser";
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/video/:filename", (req, res) => {
+  const filepath = "src/bigbuck.mp4";
+  const stat = fs.statSync(filepath);
+  const fileSize = stat.size;
+  const { range } = req.headers;
+
+  if (range) {
+    const CHUNK_SIZE = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, fileSize - 1);
+    const contentLength = end - start + 1;
+    const file = fs.createReadStream(filepath, { start, end });
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    console.log("No range in header");
+    const head = { "Content-Length": fileSize, "Content-Type": "video/mp4" };
+    res.writeHead(200, head);
+    fs.createReadStream(filepath).pipe(res);
+  }
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import app from "./app";
+
+app.listen("2000", () => {
+  console.log("server running on port 2000");
+});
