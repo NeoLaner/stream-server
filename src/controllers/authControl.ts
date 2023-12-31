85686644672fc576ca3c8511f8cb67829f72bebc
neolaner <neo.laner.official@gmail.com>
2023-12-31 16:34:08 +0330

    add instance login

--- src/controllers/authControl.ts
+++ src/controllers/authControl.ts
@@ -146,7 +146,7 @@ export const loginInstance: ExpressMiddlewareFn<void> = catchAsync(
     }
 
     createAndSendTheToken(
-      "instanceJWT",
+      "instanceJwt",
       { instance: { instanceId } },
       200,
       false,
