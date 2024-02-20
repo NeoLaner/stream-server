00edd7626fa4d9908982293132a75dc02a96655a
neolaner <neo.laner.official@gmail.com>
2024-02-20 11:57:23 +0330

    fix bug

--- src/controllers/chatControl.ts
+++ src/controllers/chatControl.ts
@@ -2,22 +2,24 @@ import mongoose from "mongoose";
 import Chat from "../models/chatModel";
 import { ExpressMiddlewareFn } from "../utils/@types";
 import { ChatDataApi, ChatRes, MessageData } from "../utils/@types/chatTypes";
-import AppError from "../utils/classes/appError";
 import catchAsync from "../utils/factory/catchAsync";
 
 export const getChat: ExpressMiddlewareFn<void> = catchAsync(
-  async function (req, res, next) {
+  async function (req, res) {
     const { instanceId } = req.params;
     const chatId = new mongoose.Types.ObjectId(instanceId);
     const chatData = await Chat.findById(chatId);
+    if (!chatData)
+      await Chat.create({
+        _id: instanceId,
+      });
+
     const chatDataWithMessages = (await chatData?.populate({
       path: "messages",
       options: { sort: { created_at: -1 }, limit: 100 }, // Optional: sort messages chronologically
     })) as ChatDataApi & { messages: MessageData[] };
 
     chatDataWithMessages.messages.reverse();
-    if (!chatData)
-      return next(new AppError("There is no instance with this id", 404));
 
     const respondData: ChatRes = {
       status: "success",
