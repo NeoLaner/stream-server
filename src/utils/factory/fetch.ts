07f93ce0a0270de917dcea6426195ff7d2d51520
neolaner <neo.laner.official@gmail.com>
2024-02-26 15:23:54 +0330

    add multiSearchTmdb

---
+++ src/utils/factory/fetch.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ofetch } from "ofetch";
+
+type P<T> = Parameters<typeof ofetch<T, any>>;
+type R<T> = ReturnType<typeof ofetch<T, any>>;
+
+const baseFetch = ofetch.create({
+  retry: 0,
+});
+
+export function mwFetch<T>(url: string, ops: P<T>[1] = {}): R<T> {
+  return baseFetch<T>(url, ops);
+}
